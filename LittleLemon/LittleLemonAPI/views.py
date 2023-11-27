b60bbf7864721531698e5f8f6e107fc8ad5be9b1
zhmurik <azhmurkova@gmail.com>
2023-11-26 18:44:47 -0800

    add role base access control

--- LittleLemon/LittleLemonAPI/views.py
+++ LittleLemon/LittleLemonAPI/views.py
@@ -1,28 +1,69 @@
 from rest_framework import generics
-from .models import MenuItem, Cart, Order, OrderItem
-from .serializers import MenuItemSerializer, CartSerializer, OrderSerializer, OrderItemSerializer
+from rest_framework.permissions import IsAuthenticated
+from rest_framework.response import Response
 
-
-class MenuItemView(generics.ListCreateAPIView):
-    queryset = MenuItem.objects.all()
-    serializer_class = MenuItemSerializer
+from .models import MenuItem, OrderItem, Order, Cart
+from .serializers import MenuItemSerializer, OrderSerializer, OrderItemSerializer, \
+    CartSerializer
 
 
 class SingleMenuItemView(generics.RetrieveUpdateDestroyAPIView, generics.CreateAPIView):
     queryset = MenuItem.objects.all()
     serializer_class = MenuItemSerializer
+    permission_classes = [IsAuthenticated]
 
+    def post(self, request, *args, **kwargs):
+        if request.user.groups.filter(name='Manager').exists():
+            return Response({"message": "Unsupported call"}, 404)
+        else:
+            return Response({"message": "Access denied"}, 403)
+
+    def put(self, request, *args, **kwargs):
+        if request.user.groups.filter(name='Manager').exists():
+            return generics.RetrieveUpdateDestroyAPIView.put(self, request, *args, **kwargs)
+        else:
+            return Response({"message": "Access denied"}, 403)
+
+    def patch(self, request, *args, **kwargs):
+        if request.user.groups.filter(name='Manager').exists():
+            return generics.RetrieveUpdateDestroyAPIView.patch(self, request, *args, **kwargs)
+        else:
+            return Response({"message": "Access denied"}, 403)
+
+    def delete(self, request, *args, **kwargs):
+        if request.user.groups.filter(name='Manager').exists():
+            return generics.RetrieveUpdateDestroyAPIView.delete(self, request, *args, **kwargs)
+        else:
+            return Response({"message": "Access denied"}, 403)
+
+class MenuItemView(generics.ListCreateAPIView):
+    queryset = MenuItem.objects.all()
+    serializer_class = MenuItemSerializer
+    permission_classes = [IsAuthenticated]
+
+    def post(self, request, *args, **kwargs):
+        if request.user.groups.filter(name='Manager').exists():
+            return generics.ListCreateAPIView.post(self, request, *args, **kwargs)
+        else:
+            return Response({"message": "Access denied"}, 403)
+
+    def put(self, request, *args, **kwargs):
+        return Response({"message": "Access denied"}, 403)
+
+    def patch(self, request, *args, **kwargs):
+        return Response({"message": "Access denied"}, 403)
+
+    def delete(self, request, *args, **kwargs):
+        return Response({"message": "Access denied"}, 403)
 
 class CartView(generics.ListCreateAPIView, generics.DestroyAPIView):
     queryset = Cart.objects.all()
     serializer_class = CartSerializer
 
+class OrderItemView(generics.RetrieveUpdateDestroyAPIView):
+    queryset = OrderItem.objects.all()
+    serializer_class = OrderItemSerializer
 
 class OrderView(generics.ListCreateAPIView):
     queryset = Order.objects.all()
     serializer_class = OrderSerializer
-
-
-class OrderItemView(generics.RetrieveUpdateDestroyAPIView):
-    queryset = OrderItem.objects.all()
-    serializer_class = OrderItemSerializer
