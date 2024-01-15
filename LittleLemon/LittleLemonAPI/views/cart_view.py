971e3e1d868cc817bedcc2234d2168473be90718
zhmurik <azhmurkova@gmail.com>
2024-01-14 22:52:12 -0800

    return single item

--- LittleLemon/LittleLemonAPI/views/cart_view.py
+++ LittleLemon/LittleLemonAPI/views/cart_view.py
@@ -7,33 +7,31 @@ from ..models import Cart, User
 from ..serializers import CartSerializer
 
 
-class CartView(generics.ListAPIView, generics.CreateAPIView, generics.DestroyAPIView):
+class CartView(generics.RetrieveDestroyAPIView, generics.CreateAPIView):
     queryset = Cart.objects.all()
     serializer_class = CartSerializer
     permission_classes = [IsAuthenticated]
 
-    def get_queryset(self):
+    def get_object(self):
         user = self.request.user
-        return Cart.objects.filter(user=user)
+        return Cart.objects.filter(user=user).first()
 
     def get(self, request, *args, **kwargs):
         if request.user.groups.filter(name__in=['Manager', 'Delivery crew']).exists():
             return Response({"message": "Access denied"}, 403)
         else:
-            return generics.ListCreateAPIView.get(self, request, *args, **kwargs)
+            return generics.RetrieveDestroyAPIView.get(self, request, *args, **kwargs)
 
     def post(self, request, *args, **kwargs):
         if request.user.groups.filter(name__in=['Manager', 'Delivery crew']).exists():
             return Response({"message": "Access denied"}, 403)
         else:
+            if self.get_object():
+                self.destroy(request, *args, **kwargs)
             return generics.CreateAPIView.post(self, request, *args, **kwargs)
-
-    def get_object(self):
-        user = self.request.user
-        return Cart.objects.filter(user=user)
 
     def delete(self, request, *args, **kwargs):
         if request.user.groups.filter(name__in=['Manager', 'Delivery crew']).exists():
             return Response({"message": "Access denied"}, 403)
         else:
-            return generics.DestroyAPIView.delete(self, request, *args, **kwargs)
+            return generics.RetrieveDestroyAPIView.delete(self, request, *args, **kwargs)
